81c0647f551946e44373ae4e1dca78c3e92be598
Justin Shaw <easyworkemail@gmail.com>
2019-04-25 13:46:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -12,3 +12,15 @@ console.log(destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups'))
 // but also, recipe is permanently alterted
  
 console.log(recipe) // { eggs: 3, flour: '3 cups' }
+
+
+
+var playlist = {  // a playlist with artist and song key-value pairs
+  biggie : '50centMcdonalds'
+};
+
+function updatePlaylist(object, artist, song) {
+  var new_playlist = Object.assign({}, object, {artist : song})
+  // Object.assign({}, playlist, { shins: 'hippie jam'})
+  return new_playlist
+}
